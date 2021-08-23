9d0a8b0c2bd2356ad0ca5f1b7efc57dbff2bba9b
LuciLua <luci.lua81@gmail.com>
2021-08-22 22:56:02 -0300

    :cactus: Color arrow + padding + if 0 decimal = nothing binario

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
 function dec(deci){
+    var saida = document.getElementById('binario')
     var deci = document.getElementById('decimal').value
+
     const binario = []
-    while(Math.floor(deci/2) >= 1){ //se a divisao for maior que 1 e se 2 for igual a 2 continu 
-        if(Math.floor(deci%2 == 0)){
-            binario.push(0)
-        }
-        else if(Math.floor(deci%2 == 1)){
-            binario.push(1)
-        }
-        deci = Math.floor(deci/2)
+
+    if (deci == 0){
+        saida.value = ''
     }
 
-    binario.push(1)
-    binario.reverse()
+    else{
+        while(Math.floor(deci/2) >= 1){ //se a divisao for maior que 1 e se 2 for igual a 2 continu 
+            if(Math.floor(deci%2 == 0)){
+                binario.push(0)
+            }
+            else if(Math.floor(deci%2 == 1)){
+                binario.push(1)
+            }
+            deci = Math.floor(deci/2)
+        }
 
-    var saida = document.getElementById('binario')
-    saida.value=binario.join('')
+        binario.push(1)
+        binario.reverse()
+    
+        saida.value = binario.join('')
+    }
 }
